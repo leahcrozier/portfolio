1974795d23096a85483a6fe3c2db9250dc5feb8f
leahcrozier <37397477+leahcrozier@users.noreply.github.com>
2023-03-21 23:00:22 +0100

    + kleine verbeteringen, vooral de homepagina

--- Website/index.js
+++ Website/index.js
@@ -17,3 +17,57 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+class FilterGallery {
+	
+	constructor(){
+		this.$filtermenuList = $('.filtermenu li');
+		this.$container      = $('.container');
+		
+		this.updateMenu('all');
+		this.$filtermenuList.on('click', $.proxy(this.onClickFilterMenu, this));
+	}
+	
+	onClickFilterMenu(event){
+		const $target      = $(event.target);
+		const targetFilter = $target.data('filter');
+
+		this.updateMenu(targetFilter);
+		this.updateGallery(targetFilter);
+	}
+	
+	updateMenu(targetFilter){
+		this.$filtermenuList.removeClass('active');
+		this.$filtermenuList.each((index, element)=>{
+			const targetData = $(element).data('filter');
+
+			if(targetData === targetFilter){
+				$(element).addClass('active');
+			}
+		})
+	}
+	
+	updateGallery(targetFilter){
+
+		if(targetFilter === 'all'){
+			this.$container.fadeOut(300, ()=>{
+				$('.post').show();
+				this.$container.fadeIn();
+			});
+		}else {
+			this.$container.find('.post').each((index, element)=>{
+				this.$container.fadeOut(300, ()=>{
+					if($(element).hasClass(targetFilter)) {
+						$(element).show();
+					}else {
+						$(element).hide();
+					}
+					this.$container.fadeIn();
+				})
+			});
+		}
+	}
+}
+
+const fliterGallery = new FilterGallery();
