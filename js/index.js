4876a18a5e6c788aadeda80280149e33af21d4c7
leahcrozier <37397477+leahcrozier@users.noreply.github.com>
2023-03-27 19:55:08 +0200

    added gallery + contact section
    :)

--- js/index.js
+++ js/index.js
@@ -19,3 +19,21 @@ function topFunction() {
 }
 
 
+// Get the modal
+var modal = document.getElementById("modal1");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("projectgallery_image");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
